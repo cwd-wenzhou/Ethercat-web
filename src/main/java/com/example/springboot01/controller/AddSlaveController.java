74f8fddf240db7358bb8873078bacae29f7456f9
cwd-wenzhou <619715109@qq.com>
2021-10-25 21:19:32 +0800

    add

--- src/main/java/com/example/springboot01/controller/AddSlaveController.java
+++ src/main/java/com/example/springboot01/controller/AddSlaveController.java
@@ -1,6 +1,8 @@
 package com.example.springboot01.controller;
 
+import com.example.springboot01.dao.SlaveInfoMapper;
 import com.example.springboot01.pojo.SlaveInfo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +11,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AddSlaveController {
+    @Autowired
+    private SlaveInfoMapper slaveInfoMapper;
+
     @GetMapping ({"/addslave","/addslave.html"})
     public String addslave(){
         return "addslave";
@@ -17,6 +22,7 @@ public class AddSlaveController {
     @PostMapping ({"/addslave","/addslave.html"})
     public String addslaveimpl(SlaveInfo slaveInfo){
         //model.getAttribute("")
+        slaveInfoMapper.InsertSlave(slaveInfo);
         System.out.println(slaveInfo);
         System.out.println("run here");
         return "redirect:/index";
